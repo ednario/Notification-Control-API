19da289ba48f508281295f3177bceffea1458bc5
Ednario Andrade <joseednario637@gmail.com>
2023-02-02 16:27:51 -0300

    test: ensure when content is greater than 240 return error

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -9,3 +9,7 @@ test('it should be able to create a notification content', () => {
 test('it should not be able to create a notification content with less than 5 characters', () => {
   expect(() => new Content('aaa')).toThrow();
 });
+
+test('it should not be able to create a notification content with more than 240 characters', () => {
+  expect(() => new Content('a'.repeat(241))).toThrow();
+});
