513b745693ea323fab7982c87928c5f70bdc2695
Ednario Andrade <joseednario637@gmail.com>
2023-02-01 22:28:51 -0300

    refactor: structure

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  getHello() {
+    return this.prisma.notification.findMany();
+  }
+}
