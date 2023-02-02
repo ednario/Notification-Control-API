a196cb7b703300f8785ba1e7e999cef8e3967718
Ednario Andrade <joseednario637@gmail.com>
2023-02-02 16:43:11 -0300

    refactor: date of creation of the optional notification in the instance

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,9 +1,10 @@
+import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
 export interface NotificationProps {
   recipientId: string;
-  content: string;
-  category: Content;
+  content: Content;
+  category: string;
   readAt?: Date | null;
   createdAt: Date;
 }
@@ -11,8 +12,11 @@ export interface NotificationProps {
 export class Notification {
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
   }
 
   public set recipientId(recipientId: string) {
@@ -23,19 +27,19 @@ export class Notification {
     return this.props.recipientId;
   }
 
-  public set content(content: string) {
+  public set content(content: Content) {
     this.props.content = content;
   }
 
-  public get content(): string {
+  public get content(): Content {
     return this.props.content;
   }
 
-  public set category(category: Content) {
+  public set category(category: string) {
     this.props.category = category;
   }
 
-  public get category(): Content {
+  public get category(): string {
     return this.props.category;
   }
 
