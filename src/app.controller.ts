78ae9194b489243527bfbffb7a52b568b806ee31
Ednario Andrade <joseednario637@gmail.com>
2023-02-01 22:38:03 -0300

    chore: listing routes and creating notifications

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
 
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello() {
+  list() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: any) {
+    const { content, category, recipientId } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
