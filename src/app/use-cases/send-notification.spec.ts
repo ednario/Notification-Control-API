78e350c30fb6498c076dcd370af12793b1d5884c
Ednario Andrade <joseednario637@gmail.com>
2023-02-03 08:45:09 -0300

    test: refactor send a notification

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,15 +1,24 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       category: 'social',
       content: 'This is notification',
       recipientId: 'Recipient id',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
