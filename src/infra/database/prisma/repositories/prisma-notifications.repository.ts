7dc3b4aa8abbba96e10b7105b921f49ef9defafe
Ednario Andrade <joseednario637@gmail.com>
2023-02-03 09:44:46 -0300

    feat: added prisma notification repository

---
+++ src/infra/database/prisma/repositories/prisma-notifications.repository.ts
@@ -0,0 +1,22 @@
+import { PrismaService } from '../prisma.service';
+
+import { NotificationRepository } from '../../../../app/repositories/notification-repository';
+
+import { Notification } from '../../../../app/entities/notification';
+
+export class PrismaNotificationRepository implements NotificationRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}
