eac6d4497ec348f07b2a02338f6473685f61869b
Ednario Andrade <joseednario637@gmail.com>
2023-02-04 13:37:57 -0300

    feat: get recipient notification

--- src/app/repositories/notification-repository.ts
+++ src/app/repositories/notification-repository.ts
@@ -2,8 +2,9 @@ import { Notification } from '@app/entities/notification';
 
 export abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
-  abstract listAll(): Promise<Array<Notification>>;
+  abstract listAll(): Promise<Notification[]>;
   abstract findById(notificationId: string): Promise<Notification | null>;
   abstract save(notification: Notification): Promise<void>;
   abstract countManyByRecipientId(recipientId: string): Promise<number>;
+  abstract findManyByRecipientId(recipientId: string): Promise<Notification[]>;
 }
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -20,6 +20,12 @@ export class InMemoryNotificationsRepository implements NotificationRepository {
     return notification;
   }
 
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
+  }
+
   async countManyByRecipientId(recipientId: string): Promise<number> {
     return this.notifications.filter(
       (notification) => notification.recipientId === recipientId,
