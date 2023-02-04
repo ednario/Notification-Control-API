73ac044487bc5bf9dae3a50b4811fd78bc433457
Ednario Andrade <joseednario637@gmail.com>
2023-02-04 14:00:32 -0300

    feat: ensure notification is read

---
+++ src/app/use-cases/read-notification.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+
+import { NotificationRepository } from '@app/repositories/notification-repository';
+import { NotificationNotFound } from '@app/use-cases/errors/notification-not-found';
+
+interface ReadNotificationRequest {
+  notificationId: string;
+}
+
+type ReadNotificationResponse = void;
+
+@Injectable()
+export class ReadNotification {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(
+    request: ReadNotificationRequest,
+  ): Promise<ReadNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.read();
+
+    await this.notificationRepository.save(notification);
+  }
+}
